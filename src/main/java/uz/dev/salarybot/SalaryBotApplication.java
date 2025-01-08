899195a5e5e1f1f484f37added51e2799104c22b
Sunnat <sunnatxidirov00@gmail.com>
2025-01-08 15:55:06 +0500

    create project

---
+++ src/main/java/uz/dev/salarybot/SalaryBotApplication.java
@@ -0,0 +1,13 @@
+package uz.dev.salarybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/dev/salarybot/SalaryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dev.salarybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
